69ea38e0ce02902ce0ba053814a0240786dd5fad
Francis Onyach <franblog7@gmail.com>
2023-04-21 07:17:14 -0700

    correct printf

--- _printf.c
+++ _printf.c
@@ -3,8 +3,8 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function that prints out given output
- * @format: format of the printed item
+ * _printf - Printf function
+ * @format: format.
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
@@ -18,7 +18,7 @@ int _printf(const char *format, ...)
 		return (-1);
 
 	va_start(list, format);
-	/*iterate through the indices */
+
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
@@ -26,7 +26,7 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &f[i], 1);*/
+			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
@@ -53,8 +53,8 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exists or NULL if not
- * @buffer: An Array of characters to be printed
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
